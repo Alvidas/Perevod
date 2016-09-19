8b1f8e7bad42c785602335b19a08ac8864dc2c71
Alvidas <preymontas@gmail.com>
2016-09-19 19:39:21 +0300

    Ver1.3

--- perevod/src/perevod/Perevod.java
+++ perevod/src/perevod/Perevod.java
@@ -35,15 +35,13 @@ public class Perevod {
             int mdes = Integer.parseInt(m, a);//Преобразование целой части.
             String mconvert = Integer.toString(mdes, c);
            
-
+            
             String fconvert = "";
             if(a==10){
+            String nul = "0.";//Преобразование дробной.
+            String fw = nul + f;
+            double fd = Double.parseDouble(fw);
                 for(int i = 0; i<4; i++){
-                    String nul = "0.";//Преобразование дробной.
-                    int fdes = Integer.parseInt(f, a);
-                    String fw = nul + f;
-                    //System.out.print(fw);
-                    double fd = Double.parseDouble(fw);
                     double s = fd*c;
                     int res = (int)s;
                     String fres = Integer.toString(res,a);
@@ -51,29 +49,27 @@ public class Perevod {
                     fconvert = fconvert + fres;
                 }
             }else{
+                int fe = f.length();
                 char[] fChar = f.toCharArray ();
-                String f1 = Character.toString(fChar[0]);
-                int i1 = Integer.parseInt(f1, a);
-                String f2 = Character.toString(fChar[1]);
-                int i2 = Integer.parseInt(f2, a);
-                String f3 = Character.toString(fChar[2]);
-                int i3 = Integer.parseInt(f3, a);
-                String f4 = Character.toString(fChar[3]);
-                int i4 = Integer.parseInt(f4, a);
-                double x1 = Math.pow(a, -1);
-                double x2 = Math.pow(a, -2);
-                double x3 = Math.pow(a, -3);
-                double x4 = Math.pow(a, -4);
-                double result = x1*i1 + x2*i2 + x3*i3 + x4*i4;
+                double result = 0;
+                for(int e = 0; e < fe; e++){
+                    String f1 = Character.toString(fChar[e]);
+                    int i1 = Integer.parseInt(f1, a);
+                    double x1 = Math.pow(a, -e);
+                    result = result + x1*i1;
+                }
                 String fcon = Double.toString(result);
                 String[] fParts = fcon.split("\\.");
-                double fd = result;
+                String nul = "0.";//Преобразование дробной.
+                String fw = nul + fParts[1];
+                double fde = Double.parseDouble(fw);
                 if(c!=10){
                     for(int i = 0; i<4; i++){
-                        double s = fd*c;
+                        double s = fde*c;
+                        System.out.println(s);
                         int res = (int)s;
-                        String fres = Integer.toString(res,a);
-                        fd = s - res; 
+                        String fres = Integer.toString(res);
+                        fde = s - res; 
                         fconvert = fconvert + fres;
                     }
                 }else{
